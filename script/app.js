67f1ca931967745c82f0aa4d29af8a96bfaa0359
desoga <done2stay@gmail.com>
2018-10-24 03:18:19 +0100

    Added another JS class called Display

--- script/app.js
+++ script/app.js
@@ -1,4 +1,4 @@
-//class initited
+//class initiated
 class AjaxWeather {
   constructor() {
     this.apiKey = '0cabe9a7ee3017fe704ccb6803b92839';
@@ -13,6 +13,21 @@ class AjaxWeather {
   }
 }
 
+//Another class initiated
+class Display {
+  constructor() {
+    this.result = document.querySelector('.results');
+    this.cityName = document.getElementById('cityName');
+    this.cityCountry = document.getElementById('cityCountry');
+    this.cityIcon = document.getElementById('cityIcon');
+    this.cityTemp = document.getElementById('cityTemp');
+    this.cityHumidity = document.getElementById('cityHumidity');
+  }
+  showWeather(data) {
+    console.log(data);
+  }
+}
+
 //Set-up of form interaction
 (function() {
   const form = document.getElementById('weatherForm');
@@ -21,13 +36,14 @@ class AjaxWeather {
 
   //class
   const ajax = new AjaxWeather(); //Instance of ajaxWeather class above
+  const display = new Display();
   form.addEventListener('submit', event => {
     event.preventDefault();
     const city = cityInput.value;
     if (city.length === 0) {
       showFeedback('City Input cannot be empty');
     } else {
-      ajax.getWeather(city).then(data => console.log(data));
+      ajax.getWeather(city).then(data => display.showWeather(data));
     }
   });
 
